75e299f227e3d69dc9a8efa98bc515c50901f26e
SachinthaWeerasinghe <weerasi-ps19259@stu.kln.ac.lk>
2023-12-31 15:31:33 +0530

    Updated XYZ.java file

--- XYZ.java
+++ XYZ.java
@@ -1,5 +1,6 @@
 class XYZ {
     public static void main(String[] args) {
         System.out.println("This is the First Java File!, I have updated it !!!"); 
+        System.out.println("Updated again!!!!-By Sachintha Weerasinghe")
     }
 }
